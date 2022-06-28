f8f316c6d0e9afabbdadcc718424ea4e8bd6c7ff
Kyle Warren <78629937+basedinbham@users.noreply.github.com>
2022-06-28 14:11:23 -0700

    final project

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kyle Warren on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
